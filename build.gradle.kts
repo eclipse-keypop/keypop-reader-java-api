8ae4d3ac367bac474ed993b44d38612fec80eb93
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-04-10 16:13:22 +0200

    clean up build

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.assertj:assertj-core:3.25.1")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 val javaSourceLevel: String by project
